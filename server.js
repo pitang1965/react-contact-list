e9bc71cf5ca88ae91e41f6eb73abfeb496405619
Shoji Makino <pitang1965@gmail.com>
2020-07-19 20:52:15 +0900

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,19 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extend: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactList API...' }));
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in puroduction
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
