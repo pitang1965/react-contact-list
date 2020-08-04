695ae44d615a1d54d3e64f8af5c7d1847b1ae7c9
Shoji Makino <pitang1965@gmail.com>
2020-08-04 18:16:53 +0900

    add information to About page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 function About() {
   return (
     <div>
       <h1>このアプリについて</h1>
-      <p className='my-1'>
-        これは連絡先を保管するフルスタックアプリケーションです。
+      <p>これは連絡先を保管するフルスタックアプリケーションです。</p>
+      <p>
+        このアプリのソースコードは
+        <a href='https://github.com/pitang1965/react-contact-list'>GitHub</a>
+        にあります。
       </p>
-      <p className='bg-dark p'><strong>Version: </strong>1.0.0</p>
+      <p>
+        詳細記事は<a href='https://software.pitang1965.com/'>ブログ</a>
+        にあげるかも。
+      </p>
+      <p>
+        作成者：ピータン<a href='https://twitter.com/pitang1965'>(Twitter)</a>
+      </p>
+      <br />
+      <p className='bg-dark p'>おまけ</p>
+      <p>我が家のメダカです。よろしければご覧ください。</p>
+      <iframe
+        title='Tomora'
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/4e9PS8PYrHo'
+        frameborder='0'
+        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+        allowfullscreen
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
