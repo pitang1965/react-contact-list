46021524da5badcc9f991d751a83be34412f3f7f
pitang1965 <pitang1965@gmail.com>
2022-11-27 23:12:08 +0900

    🐛 no longer supported options were used

--- config/db.js
+++ config/db.js
@@ -4,12 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(db);
 
     console.log('MongoDB Connected...');
   } catch (err) {
